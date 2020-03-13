5238f126d294283a5374775d024bcea77d6a090f
Souhail El Maslouhi <48288703+SouhailELM@users.noreply.github.com>
2020-03-13 11:38:11 +0100

    initial

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Buttons from './Buttons';
+
+function App() {
+  return (
+    <div>
+      <Buttons />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Buttons.tsx
@@ -0,0 +1,89 @@
+import React, { useState } from 'react';
+import ToggleButtonGroup from '@material-ui/lab/ToggleButtonGroup';
+import ToggleButton from '@material-ui/lab/ToggleButton';
+import { makeStyles, withStyles, Theme, createStyles } from '@material-ui/core/styles';
+
+
+
+function Buttons() {
+
+    const ButtonsGroup = [
+        {
+            label: 'Homestay',
+            value: 'homestay'
+        },
+        {
+            label: 'Hotel',
+            value: 'hotel'
+        },
+        {
+            label: 'Villa',
+            value: 'villa'
+        },
+        {
+            label: 'Resort',
+            value: 'resort'
+        }
+    ]
+
+    const [buttonValue, setButtonValue] = useState('');
+
+    const useStyles = makeStyles({
+        root: {
+            background: '#FFFFFF',
+            borderRadius: 6,
+        },
+        label: {
+
+            fontStyle: 'normal',
+            fontWeight: 'normal',
+            fontSize: 13,
+            color: '#1A051D',
+            display: 'flex',
+            alignItems: 'center'
+        },
+        selected: {
+            background: 'linear-gradient(199.65deg, #BD7AE3 100%, #8461C9 0%)',
+            borderRadius: 6,
+            color: '#FFFFFF'
+        }
+    })
+    const classes = useStyles();
+
+    const StyledToggleButtonGroup = withStyles(theme => ({
+        grouped: {
+            margin: theme.spacing(0.5),
+            border: 'none',
+            padding: theme.spacing(0, 1),
+            '&:not(:first-child)': {
+                borderRadius: theme.shape.borderRadius,
+            },
+            '&:first-child': {
+                borderRadius: theme.shape.borderRadius,
+            },
+        },
+    }))(ToggleButtonGroup);
+
+    const handleClick = (e: React.MouseEvent<HTMLElement>, newValue: string) => {
+        setButtonValue(newValue);
+    }
+
+    return (
+        <div>
+            <StyledToggleButtonGroup value={buttonValue} onChange={handleClick} exclusive>
+                {
+                    ButtonsGroup.map((button, i: number) => (
+                        <ToggleButton classes={{
+                            root: classes.root,
+                            label: classes.label,
+                            selected: classes.selected
+                        }} value={button.value} key={i} >{button.label}</ToggleButton>
+                    ))
+                }
+            </StyledToggleButtonGroup>
+        </div>
+    );
+}
+
+
+export default Buttons;
